05d64333eba938307eef9e306ef7c2f787bf14cb
Cesar Augusto Diaz Arriaga <cesar.a.diaz@gmail.com>
2021-08-09 18:25:48 -0500

    Conexion a base de datos exitosa

--- src/main/java/co/edu/utp/misiontic2022/c2/App.java
+++ src/main/java/co/edu/utp/misiontic2022/c2/App.java
@@ -1,13 +1,22 @@
 package co.edu.utp.misiontic2022.c2;
 
+import java.sql.SQLException;
+
+import co.edu.utp.misiontic2022.c2.util.JDBCUtilities;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        try {
+            var conn = JDBCUtilities.getConnection();
+            System.out.println("Conexion realizada exitosamente");
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println("Error: " + e);
+            e.printStackTrace();
+        }
     }
 }
